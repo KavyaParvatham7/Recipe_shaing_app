de99a32b75da6d357114ba52fc8ad9a4f7c6a5dc
Kavya Parvatham <kavyaparvatham7@gmail.com>
2025-04-04 18:41:42 +0530

    Changed backend urls in frontend pages

--- frontend/src/pages/create_recipe.js
+++ frontend/src/pages/create_recipe.js
@@ -40,7 +40,7 @@ export const CreateRecipe = () => {
     event.preventDefault();
     try {
       await axios.post(
-        "https://recipe-app-kh93.onrender.com/recipes",
+        "https://recipe-sharing-app-hbjz.onrender.com/recipes",
         { ...recipe },
         {
           headers: { authorization: cookies.access_token },
